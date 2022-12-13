25e97d594cc1f756d53c36ad01f9fc7de8c324ce
Darren McElligott <darren.mcelligott@gmail.com>
2022-12-13 09:49:21 -0500

    day 8 part 2

--- 8/index.ts
+++ 8/index.ts
@@ -31,7 +31,30 @@ function setVisibility(visible: Set<string>, source: number[][], transposeGrid: 
     }
   }
 }
+function calcScore(size: number, sight: number[]): number {
+  let score = 0;
+  for (const tree of sight) {
+    score++;
+    if (tree >= size) break;
+  }
+  return score;
+}
 
+function mapMultIncr(map: Map<string, number>, key: string, val: number) {
+  map.set(key, map.has(key) ? map.get(key) * val : val);
+}
+
+function setScores(scores: Map<string, number>, source: number[][], transposeGrid: boolean) {
+  const grid = transposeGrid ? transpose(source) : source;
+  for (let rownum = 0; rownum < grid.length; rownum++) {
+    const row = grid[rownum];
+    for (let left = 0; left < row.length; left++) {
+      const right = row.length - left - 1;
+      mapMultIncr(scores,transposeGrid ? `${left},${rownum}`: `${rownum},${left}`, calcScore(row[left], row.slice(left + 1)));
+      mapMultIncr(scores,transposeGrid ? `${right},${rownum}`: `${rownum},${right}`, calcScore(row[right], row.slice(0, right).reverse()));
+    }
+  }
+}
 function part1(input: string) {
   const grid = parseInput(input);
   const visible = new Set<string>();
@@ -40,8 +63,15 @@ function part1(input: string) {
   return visible.size;
 }
 
+function part2(input: string) {
+  const grid = parseInput(input);
+  const scores = new Map<string, number>();
+  setScores(scores, grid, false);
+  setScores(scores, grid, true);
+  return Math.max(...scores.values());
+}
 // console.log(part1(dataSets.test1));
 console.log('part1:', part1(dataSets.input));
 
 // console.log(part2(dataSets.test1));
-// console.log('part2:', part2(dataSets.input));
+console.log('part2:', part2(dataSets.input));
